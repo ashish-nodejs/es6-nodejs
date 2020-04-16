6424c7d3c9c4dcca155b24b0fd9fceb6c274f7e3
ashish-ajm <vickys9461@gmail.com>
2020-04-16 13:56:08 +0530

    add view file and structure update

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-	res.render('index', { title: 'World' });
+	res.render('./view/index', { title: 'World' });
 });
 
 export default router;
